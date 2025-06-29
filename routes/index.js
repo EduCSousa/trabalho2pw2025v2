5604335e8c6af7dc0b7c80a762d7df6db31a33b9
EduCSousa <edsousa@ipvc.pt>
2025-06-29 15:27:38 +0100

    Segundo Commit do trabalho2-mashup-apis

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,15 @@ function isAuth(req, res, next) {
   res.redirect('/login');
 }
 
+// Rota para a raiz '/'
+router.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect('/dashboard');
+  } else {
+    res.redirect('/login');
+  }
+});
+
 router.get('/dashboard', isAuth, async (req, res) => {
   const history = await Search.find({ userId: req.user._id }).sort({ timestamp: -1 }).limit(5);
   res.render('dashboard', { user: req.user, results: null, history });
--- server.js
+++ server.js
@@ -9,9 +9,12 @@ const app = express();
 
 // DB + Auth
 require('./config/passport')(passport);
-mongoose.connect(process.env.MONGODB_URI);
 
-// Middlewares
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB conectado!'))
+  .catch(err => console.error('Erro na ligação ao MongoDB:', err));
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
@@ -24,10 +27,21 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Routes
+
 app.use('/', require('./routes/auth'));
 app.use('/', require('./routes/index'));
 app.use('/api', require('./routes/api'));
 
-// Server
+
+app.use((req, res) => {
+  res.status(404).send('Página não encontrada');
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Erro no servidor!');
+});
+
+
 app.listen(3000, () => console.log('Servidor em http://localhost:3000'));
